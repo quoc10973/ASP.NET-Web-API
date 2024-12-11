85013a68ce2b1b2cc197ac8959ef0ab7348157e2
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-11 19:55:22 +0700

    entity + dbcontext

---
+++ ASPWebApp/Entities/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPWebApp.Entities
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+        
+        public string Author { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ ASPWebApp/Entities/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPWebApp.Entities
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book>? Books { get; set; }
+    }
+   
+}
