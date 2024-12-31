f6e6317494f1ed657ace22afde295cc77b1684e2
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2024-12-31 19:50:16 +0700

    ProductController

---
+++ ASPWebApp/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using ASPWebApp.Entities;
+using ASPWebApp.Model;
+using ASPWebApp.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public ProductController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBook()
+        {
+            try
+            {
+                return Ok(await _bookRepository.GetAllBooksAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetBookById(int id)
+        {
+            try
+            {
+                return Ok(await _bookRepository.GetBookByIdAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddBook(BookDTO book)
+        {
+            try
+            {
+                return Ok(await _bookRepository.AddBookAsync(book));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public async Task<IActionResult> UpdateBook(int id, BookDTO book)
+        {
+            try
+            {
+                return Ok(await _bookRepository.UpdateBookAsync(id, book));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            try
+            {
+                await _bookRepository.DeleteBookAsync(id);
+                return Ok("Deleted !");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
