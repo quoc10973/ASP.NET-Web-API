6c361ce5323502278fa57a07a2d4f6b6e8710bec
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-02 18:39:03 +0700

    order Irepo

---
+++ ASPWebApp/Repository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using ASPWebApp.Entities;
+
+namespace ASPWebApp.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync();
+        Task<Order> GetOrderByIdAsync(Guid id);
+        Task AddOrderAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(Order order);
+
+    }
+}
